9d9e874e5434312db3237742ba70105b96cc9779
Alessandro Sordoni <alsordon@microsoft.com>
2025-04-15 13:29:11 -0700

    3b scripts

---
+++ scripts/grpo_cd_3b.sh
@@ -0,0 +1,29 @@
+# This will use 2 GPUs total: 1 process to train the model + 1 SGL GPU to do online inferencing
+
+# TRAIN_PROCS=1    : num training processes
+# SGL_BASE_GPU_ID=1: puts SGL server in GPU 1
+# SGL_DP_SIZE=1    : means that SGL will use 1 GPU
+
+export TRAIN_PROCS=1
+
+DIR="/data/alsordon/runs_outputs"
+NAME="grpo_cd"
+
+torchrun --nproc-per-node=${TRAIN_PROCS} run_torch.py \
+    -m q3i \
+    -o ${DIR}/${NAME}_42 \
+    -t 1.0 \
+    -k 4 \
+    --lr 5e-6 \
+    -a grpo \
+    --dataset cd \
+    --onlbsz 16 \
+    --offbsz 8 \
+    --innbsz 1 \
+    --maxtok 1024 \
+    --maxepochs 1000 \
+    --offepc 1 \
+    -s 42 \
+    --kl_ctl 0.001 \
+    --evalevery 25 \
+    --opt adamw8bit
