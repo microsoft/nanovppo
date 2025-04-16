dd90a84a289d1c64b3ca470ce227d0df7a588457
Alessandro Sordoni <alessandro.sordoni@gmail.com>
2025-04-16 17:56:22 -0400

    Create grpo_cd_3b.sh

---
+++ scripts/grpo_cd_3b.sh
@@ -0,0 +1,23 @@
+export TRAIN_PROCS=1
+
+DIR="./runs_outputs"
+NAME="grpo_cd_3b"
+
+torchrun --nproc-per-node=${TRAIN_PROCS} run_torch.py \
+    -m q3i \
+    -o ${DIR}/${NAME}_42 \
+    -t 1.0 \
+    -k 4 \
+    --lr 5e-6 \
+    -a grpo \
+    --dataset cd \
+    --onlbsz 16 \
+    --offbsz 8 \
+    --innbsz 1 \
+    --maxtok 1024 \
+    --maxepochs 1000 \
+    --offepc 1 \
+    -s 42 \
+    --kl_ctl 0.001 \
+    --evalevery 25 \
+    --opt adamw8bit
